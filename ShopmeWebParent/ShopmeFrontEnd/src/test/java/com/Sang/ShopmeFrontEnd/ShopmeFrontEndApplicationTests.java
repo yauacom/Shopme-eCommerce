082382cf0d713856e7eeac84b51cf397e5515cf7
yauacom <70412615+yauacom@users.noreply.github.com>
2021-07-20 18:02:43 -0700

    project initialize

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/Sang/ShopmeFrontEnd/ShopmeFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sang.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/Sang/ShopmeProject/ShopmeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Sang.ShopmeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmeProjectApplication.class, args);
+	}
+
+}
