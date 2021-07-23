51cfa3c91a471ea86db4f81b97147ffbd6df2084
yauacom <70412615+yauacom@users.noreply.github.com>
2021-07-23 13:21:25 -0700

    Added save new user

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/Sang/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/Sang/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.Sang.admin.user;
 
+import com.Sang.ShopmeCommon.entity.Role;
 import com.Sang.ShopmeCommon.entity.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +10,24 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
   private UserRepository userRepository;
+  private RoleRepository roleRepository;
 
   @Autowired
-  public UserService(UserRepository userRepository) {
+  public UserService(UserRepository userRepository,
+      RoleRepository roleRepository) {
     this.userRepository = userRepository;
+    this.roleRepository = roleRepository;
   }
 
   public List<User> listAll() {
     return (List<User>) userRepository.findAll();
+  }
+
+  public List<Role> listRoles() {
+    return (List<Role>) roleRepository.findAll();
+  }
+
+  public void save(User newUser) {
+    userRepository.save(newUser);
   }
 }
