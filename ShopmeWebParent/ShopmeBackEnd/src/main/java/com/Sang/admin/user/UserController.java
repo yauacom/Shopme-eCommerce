f64640ecb3fdb06063a2047e5cc6524bc61b8d93
yauacom <70412615+yauacom@users.noreply.github.com>
2021-08-04 15:13:21 -0700

    added function to update user information

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/Sang/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/Sang/admin/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -36,6 +37,7 @@ public class UserController {
 
     model.addAttribute("user", user);
     model.addAttribute("listRoles", listRoles);
+    model.addAttribute("pageTitle", "Create New User");
     return "user_form";
   }
 
@@ -47,5 +49,22 @@ public class UserController {
     redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
 
     return "redirect:/users";
+  }
+
+  @GetMapping("/users/edit/{id}")
+  public String editUser(@PathVariable(name = "id") Integer id,
+      Model model,
+      RedirectAttributes redirectAttributes) {
+    try {
+      User user = userService.get(id);
+      List<Role> listRoles = userService.listRoles();
+      model.addAttribute("listRoles", listRoles);
+      model.addAttribute("user", user);
+      model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+      return "user_form";
+    } catch (UserNotFoundException ex) {
+      redirectAttributes.addFlashAttribute("message", ex.getMessage());
+      return "redirect:/users";
+    }
   }
 }
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/Sang/admin/user/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.Sang.admin.user;
+
+public class UserNotFoundException extends Exception {
+
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+}
