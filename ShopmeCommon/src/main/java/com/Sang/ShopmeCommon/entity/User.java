e2213873b04c33f0cf5c7e0d3695f34bcc3fd7a5
yauacom <70412615+yauacom@users.noreply.github.com>
2021-08-07 00:48:05 -0700

    added user authentication, log in and log out function

--- ShopmeCommon/src/main/java/com/Sang/ShopmeCommon/entity/User.java
+++ ShopmeCommon/src/main/java/com/Sang/ShopmeCommon/entity/User.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -50,7 +51,7 @@ public class User {
 
 
 
-  @ManyToMany
+  @ManyToMany (fetch = FetchType.EAGER)
   @JoinTable(
       name = "users_roles",
       joinColumns = @JoinColumn(name = "user_id"),
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/Sang/admin/security/ShopmeUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.Sang.admin.security;
+
+import com.Sang.ShopmeCommon.entity.User;
+import com.Sang.admin.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class ShopmeUserDetailsService implements UserDetailsService {
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    User user = userRepository.getUserByEmail(email);
+    if (user != null) return new ShopmeUserDetails(user);
+    throw new UsernameNotFoundException("Could not find user with email: " + email);
+  }
+}
